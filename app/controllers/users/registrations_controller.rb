2c178747d612e45ce637353bb6e35a3b357b0c3d
Craig Lu <Craig.Z.Lu@gmail.com>
2017-04-06 14:18:30 -0400

    display joined date on user profile and redirect user to profile after editting their profile

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -19,7 +19,25 @@ before_action :configure_account_update_params, only: [:update]
 
   # PUT /resource
   def update
-    super
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+    if resource_updated
+      if is_flashing_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+          :update_needs_confirmation : :updated
+        set_flash_message :success, flash_key
+      end
+      bypass_sign_in resource, scope: resource_name
+      # respond_with resource, location: after_update_path_for(resource)
+      redirect_to user_profile_path(current_user.id)
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
   end
 
   # DELETE /resource
